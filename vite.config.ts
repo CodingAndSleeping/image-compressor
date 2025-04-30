f9db8b137b2567513b2f37719dbd0b4befd4e8ae
lzt <1162300237@qq.com>
2025-05-01 01:37:57 +0800

    feat: image compressor

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/image-compressor',
+  base: '/image-compressor/',
   plugins: [vue()],
   resolve: {
     alias: {
